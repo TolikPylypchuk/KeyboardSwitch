ae2ad1e963e01437b74c37cafa4e62897c1a18bd
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2017-12-29 00:10:24 +0200

    Add basic installer

--- KeyboardSwitch/Properties/Resources.Designer.cs
+++ KeyboardSwitch/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace KeyboardSwitch.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Text {
+            get {
+                object obj = ResourceManager.GetObject("Text", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
