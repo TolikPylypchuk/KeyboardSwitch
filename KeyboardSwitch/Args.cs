5744c6deb4132b4b54a5b5ed7cb84d2fdce6574f
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2018-01-13 18:53:44 +0200

    Make so that the app starts when the user logs in

--- KeyboardSwitch/Args.cs
+++ KeyboardSwitch/Args.cs
@@ -2,7 +2,7 @@
 {
 	public static class Args
 	{
-		public const string NO_WINDOW = "--nowindow";
+		public const string NO_WINDOW = "--no-window";
 		public const string FORWARD = "--forward";
 		public const string BACKWARD = "--backward";
 		public const string EXIT = "--exit";
