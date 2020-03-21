3bc8fea14830c5a74460f99b59371022a446a516
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-03-21 13:56:59 +0200

    Start creating the infrastructure
     - Add a project for common things
    
     - Add ReactiveUI

--- KeyboardSwitch.Settings/MainWindow.xaml.cs
+++ KeyboardSwitch.Settings/MainWindow.xaml.cs
@@ -1,12 +1,25 @@
-using System.Windows;
+using System.Reactive.Disposables;
+
+using KeyboardSwitch.Settings.ViewModels;
+
+using ReactiveUI;
 
 namespace KeyboardSwitch.Settings
 {
-    public partial class MainWindow : Window
+    public abstract class MainWindowBase : ReactiveWindow<MainViewModel> { }
+
+    public partial class MainWindow : MainWindowBase
     {
         public MainWindow()
         {
             this.InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(v => v.ViewModel)
+                    .BindTo(this, v => v.DataContext)
+                    .DisposeWith(disposables);
+            });
         }
     }
 }
---
+++ KeyboardSwitch.Settings/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+using ReactiveUI;
+
+namespace KeyboardSwitch.Settings.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+    }
+}
