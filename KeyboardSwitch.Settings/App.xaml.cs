5145402bf628097ebdd003ee28caee09bc5d98bd
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-04-29 00:38:05 +0300

    Implement getting window size and location from the settings

--- KeyboardSwitch.Settings/App.xaml.cs
+++ KeyboardSwitch.Settings/App.xaml.cs
@@ -19,18 +19,28 @@ namespace KeyboardSwitch.Settings
         public override void Initialize()
             => AvaloniaXamlLoader.Load(this);
 
-        public override void OnFrameworkInitializationCompleted()
+        public override async void OnFrameworkInitializationCompleted()
         {
             this.Log().Info("Starting the settings app");
 
             if (this.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                var settings = await Locator.Current.GetService<ISettingsService>().GetUISettingsAsync();
+
                 var mainWindow = new MainWindow
                 {
-                    ViewModel = new MainViewModel()
+                    ViewModel = new MainViewModel(),
+                    Width = settings.WindowWidth,
+                    Height = settings.WindowHeight
                 };
 
+                if (settings.WindowX >= 0 && settings.WindowY >= 0)
+                {
+                    mainWindow.Position = new PixelPoint(settings.WindowX, settings.WindowY);
+                }
+
                 desktop.MainWindow = mainWindow;
+                desktop.MainWindow.Show();
 
                 desktop.Exit += this.OnExit;
             }
