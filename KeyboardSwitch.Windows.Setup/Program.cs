7c3c1dd32366d67a5fbc83718543ac8be1f293ef
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-06-27 18:03:49 +0300

    Implement publishing the app before building the setup

--- KeyboardSwitch.Windows.Setup/Program.cs
+++ KeyboardSwitch.Windows.Setup/Program.cs
@@ -1,17 +1,26 @@
 using System;
+using System.IO;
 
 using WixSharp;
 using WixSharp.Forms;
+
+using Assembly = System.Reflection.Assembly;
 
 namespace KeyboardSwitch.Windows.Setup
 {
     public class Program
     {
+        private const string BuildDirectory = @"..\bin\publish\*.*";
+        private static readonly string TargetDirectory = @$"%ProgramFiles%\{nameof(KeyboardSwitch)}";
+
+        private const string MsiName = "KeyboardSwitchSetup.msi";
+        private static readonly string MsiPath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetLocation()), MsiName);
+
         private static void Main()
         {
             var project = new ManagedProject(nameof(KeyboardSwitch),
-                             new Dir(@$"%ProgramFiles%\{nameof(KeyboardSwitch)}",
-                                 new File($"{nameof(Program)}.cs")))
+                             new Dir(TargetDirectory, new Files(BuildDirectory)))
             {
                 GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b"),
                 ManagedUI = ManagedUI.Default
@@ -28,7 +37,7 @@ namespace KeyboardSwitch.Windows.Setup
             project.BeforeInstall += MsiBeforeInstall;
             project.AfterInstall += MsiAfterInstall;
 
-            project.BuildMsi();
+            project.BuildMsi(MsiPath);
         }
 
         private static void MsiLoad(SetupEventArgs e)
