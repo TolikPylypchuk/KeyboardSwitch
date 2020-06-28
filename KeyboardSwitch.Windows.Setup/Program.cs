964cf9a8e11e7d67c65efa58bafdfdd1491359a9
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-06-28 22:56:00 +0300

    Implement the installer for the app
    Implement installing the app. Most of the functionality is ready, except
    for configuring it to run at system startup, and removing configuration
    during uninstallation.
    
    Also, fix the BadImageFormatException in the settings app by removing a
    dependency on ReactiveUI.Fody from it (it's needed only for the settings
    core project).

--- KeyboardSwitch.Windows.Setup/Program.cs
+++ KeyboardSwitch.Windows.Setup/Program.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 using WixSharp;
-using WixSharp.Forms;
 
 using Assembly = System.Reflection.Assembly;
 
@@ -10,46 +12,79 @@ namespace KeyboardSwitch.Windows.Setup
 {
     public class Program
     {
-        private const string BuildDirectory = @"..\bin\publish\*.*";
-        private static readonly string TargetDirectory = @$"%ProgramFiles%\{nameof(KeyboardSwitch)}";
+        private const string ProductName = "Keyboard Switch";
+        private const string ProductSettingsName = "Keyboard Switch Settings";
 
-        private const string MsiName = "KeyboardSwitchSetup.msi";
-        private static readonly string MsiPath = Path.Combine(
-            Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetLocation()), MsiName);
+        private const string BuildDirectory = @"..\bin\publish\*.*";
+        private static readonly string TargetDirectory = @$"%ProgramFiles64%\{nameof(KeyboardSwitch)}";
+        private const string InstallationDirectory = "[INSTALLDIR]";
+        private const string StartMenuDirectory = "%ProgramMenu%";
+
+        private const string SettingsAppName = "KeyboardSwitchSettings.exe";
 
         private static void Main()
         {
-            var project = new ManagedProject(nameof(KeyboardSwitch),
-                             new Dir(TargetDirectory, new Files(BuildDirectory)))
+            var target = new Dir(TargetDirectory, new Files(BuildDirectory));
+
+            var startMenuShortcut = new Dir(
+                StartMenuDirectory,
+                new ExeFileShortcut(
+                    ProductSettingsName, Path.Combine(InstallationDirectory, SettingsAppName), arguments: String.Empty)
+                {
+                    WorkingDirectory = InstallationDirectory
+                });
+
+            var currentAssembly = Assembly.GetExecutingAssembly();
+
+            var project = new ManagedProject(ProductName, target, startMenuShortcut)
             {
                 GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b"),
-                ManagedUI = ManagedUI.Default
+                InstallScope = InstallScope.perMachine,
+                InstallerVersion = 500,
+                InstallPrivileges = InstallPrivileges.elevated,
+                Platform = Platform.x64,
+                OutDir = Path.GetDirectoryName(currentAssembly.GetLocation()),
+                LicenceFile = "License.rtf",
+                BannerImage = "Banner.bmp",
+                BackgroundImage = "Dialog.bmp",
+                ValidateBackgroundImage = false,
+                MajorUpgradeStrategy = MajorUpgradeStrategy.Default,
+                UI = WUI.WixUI_InstallDir,
+                Version = currentAssembly.GetName().Version
             };
 
-            project.ManagedUI.InstallDialogs
-                .Add(Dialogs.Welcome)
-                .Add(Dialogs.Licence)
-                .Add(Dialogs.InstallDir)
-                .Add(Dialogs.Progress)
-                .Add(Dialogs.Exit);
+            project.ControlPanelInfo.NoModify = true;
+            project.ControlPanelInfo.NoRepair = true;
+            project.ControlPanelInfo.ProductIcon = "icon.ico";
+            project.ControlPanelInfo.InstallLocation = InstallationDirectory;
 
-            project.Load += MsiLoad;
-            project.BeforeInstall += MsiBeforeInstall;
-            project.AfterInstall += MsiAfterInstall;
+            project.ControlPanelInfo.Manufacturer = currentAssembly
+                .GetCustomAttributes(typeof(AssemblyCompanyAttribute))
+                .OfType<AssemblyCompanyAttribute>()
+                .First()
+                .Company;
 
-            project.BuildMsi(MsiPath);
+            project.AfterInstall += AfterInstall;
+
+            project.BuildMsi();
         }
 
-        private static void MsiLoad(SetupEventArgs e)
+        private static void AfterInstall(SetupEventArgs e)
         {
-        }
-
-        private static void MsiBeforeInstall(SetupEventArgs e)
-        {
-        }
-
-        private static void MsiAfterInstall(SetupEventArgs e)
-        {
+            if (!e.IsUninstalling)
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = Path.Combine(e.InstallDir, SettingsAppName),
+                        WorkingDirectory = e.InstallDir
+                    });
+                } catch (Exception exp)
+                {
+                    e.Session.Log($"An exception occured when trying to start the Keyboard Switch Settings app: {exp}");
+                }
+            }
         }
     }
 }
