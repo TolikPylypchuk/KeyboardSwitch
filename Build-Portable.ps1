799b91bf72e188a80590c69aca8cefacd90475d5
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2020-07-03 20:45:17 +0300

    Add a script which builds the portable version of the app

---
+++ Build-Portable.ps1
@@ -0,0 +1,18 @@
+if (Test-Path .\bin\KeyboardSwitch\) 
+{
+    Remove-Item -Path .\bin\KeyboardSwitch\ -Recurse
+}
+
+dotnet publish .\KeyboardSwitch\ --configuration Release --runtime win10-x64 --self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+dotnet publish .\KeyboardSwitch.Settings\ --configuration Release --runtime win10-x64 --self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+
+Remove-Item -Path .\bin\KeyboardSwitch\ -Include *.pdb, *.xml
+
+if (Test-Path .\bin\KeyboardSwitch-Portable.zip) 
+{
+    Remove-Item -Path .\bin\KeyboardSwitch-Portable.zip
+}
+
+Compress-Archive -Path .\bin\KeyboardSwitch\ -DestinationPath .\bin\KeyboardSwitch-Portable.zip
+
+Remove-Item -Path .\bin\KeyboardSwitch\ -Recurse
