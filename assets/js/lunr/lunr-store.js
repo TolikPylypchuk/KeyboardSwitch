bcaea8cb828cfcf946e701f392bac160e8a82251
TolikPylypchuk <TolikPylypchuk@users.noreply.github.com>
2021-01-01 16:24:19 +0000

    jekyll build from Action 05c04d413e31bbb6820512e818c862afb24b87e4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Keyboard Switch 3.0",
+        "excerpt":"Welcome to the Keyboard Switch 3.0 docs! Keyboard Switch is an application which switches typed text as if it were typed with another keyboard layout. If you ever write some text and then realise that you have written it using a wrong layout, you don’t have to delete it and...","categories": [],
+        "tags": [],
+        "url": "/v3.0/",
+        "teaser": null
+      },{
+        "title": "Installation",
+        "excerpt":"Using a Windows Installer You can get the latest version of the app from the releases page on GitHub. Download the .msi file and run it to install the app. After installing the app, the installer will run the settings app. Currently it’s not really optimized, so the startup time...","categories": [],
+        "tags": [],
+        "url": "/v3.0/installation",
+        "teaser": null
+      },{
+        "title": "The Settings App",
+        "excerpt":"Keyboard Switch Settings is a simple app which controls the behavior of the Keyboard Switch service. If you installed the app using a Windows installer, you can find the app in the Start menu. Here’s how it looks: The app is composed of six tabs: Character mappings: contains the settings...","categories": [],
+        "tags": [],
+        "url": "/v3.0/settings-app",
+        "teaser": null
+      },{
+        "title": "Character Mappings",
+        "excerpt":"In order for the Keyboard Switch service to know how to map characters, you have to configure the character mappings yourself. The app reads the list of installed layouts in the system, so it knows which layouts you use and in what order. What it doesn’t (and can’t) know is...","categories": [],
+        "tags": [],
+        "url": "/v3.0/char-mappings",
+        "teaser": null
+      },{
+        "title": "Preferences",
+        "excerpt":"The Preferences tab contains other settings which control the behavior of the Keyboard Switch service. It has two main looks, based on the switch mode setting: Following is the discussion on the many fields that are in this tab. Check-Boxes There are four check-boxes at the top of the Preferences...","categories": [],
+        "tags": [],
+        "url": "/v3.0/preferences",
+        "teaser": null
+      },{
+        "title": "Converter",
+        "excerpt":"Converter lets you manually convert text between arbitrary layouts, not just those installed in your system. Note Converter is a very niche feature. Chances are that you’ll never need to use it and never even think of using it. Moreover, it has nothing to do with the main functionality of...","categories": [],
+        "tags": [],
+        "url": "/v3.0/converter",
+        "teaser": null
+      },{
+        "title": "Other Tabs",
+        "excerpt":"Other tabs in the settings app include the Sandbox tab and the About tab. Sandbox Sandbox is absurdly simple - it’s just a text box. That’s it! Here’s how it looks: Sandbox lets you play around with switching text when you change the app’s settings. It contains no logic whatsoever....","categories": [],
+        "tags": [],
+        "url": "/v3.0/other-tabs",
+        "teaser": null
+      },{
+        "title": "Technical Stuff",
+        "excerpt":"Keyboard Switch is written using C# and .NET Core 3.1. Following is the list of technologies used in this app and some other technical aspects. UI The Keyboard Switch Settings app is written using Avalonia, a cross-platform UI framework for .NET. I chose it because unlike other UI frameworks for...","categories": [],
+        "tags": [],
+        "url": "/v3.0/technical",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"Keyboard Switch: version 3.0 Created by Tolik Pylypchuk. Acknowledgements Icon made by Smashicons from www.flaticon.com. The UI for the settings app was created using Avalonia with ReactiveUI and the Citrus theme by worldbeater. Docs are built using Jekyll with the Minimal Mistakes theme by Michael Rose. Changelog v3.0 (4 August...","categories": [],
+        "tags": [],
+        "url": "/v3.0/about",
+        "teaser": null
+      }]
