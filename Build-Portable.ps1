c212dc010e7e5e4e443023337070c5bf0329cba2
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-01-27 23:44:18 +0200

    Update the publish scripts to include the active TFM

--- Build-Portable.ps1
+++ Build-Portable.ps1
@@ -3,8 +3,11 @@ if (Test-Path .\bin\KeyboardSwitch\)
     Remove-Item -Path .\bin\KeyboardSwitch\ -Recurse
 }
 
-dotnet publish .\KeyboardSwitch\ --configuration Release --runtime win10-x64 --self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
-dotnet publish .\KeyboardSwitch.Settings\ --configuration Release --runtime win10-x64 --self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+dotnet publish .\KeyboardSwitch\ --configuration Release --runtime win10-x64 --framework net5.0-windows `
+--self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+
+dotnet publish .\KeyboardSwitch.Settings\ --configuration Release --runtime win10-x64 --framework net5.0-windows `
+--self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
 
 Remove-Item -Path .\bin\KeyboardSwitch\ -Include *.pdb, *.xml
 
