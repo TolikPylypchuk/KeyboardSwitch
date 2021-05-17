09e510a445609aeacbe9b3a212675cf5ba2e29c7
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-05-17 23:54:45 +0300

    Add a build script for Linux
    Translate the PowerShell script used for building the portable Windows
    version to Bash so that a Linux version can be built more easily.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm -rf ./bin/KeyboardSwitch 2> /dev/null
+mkdir -p ./bin
+cd ./bin
+
+dotnet publish ../KeyboardSwitch --configuration Release --runtime linux-x64 --framework net5.0 \
+--self-contained true --output ./KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+
+dotnet publish ../KeyboardSwitch.Settings --configuration Release --runtime linux-x64 --framework net5.0 \
+--self-contained true --output ./KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
+
+find ./KeyboardSwitch -name "*.pdb" -type f -delete
+find ./KeyboardSwitch -name "*.xml" -type f -delete
+find ./KeyboardSwitch -name "*.dylib" -type f -delete
+rm ./KeyboardSwitch/uiohook.dll
+
+rm ./KeyboardSwitch.tar.gz 2> /dev/null
+
+tar -czf ./KeyboardSwitch.tar.gz ./KeyboardSwitch
+
+rm -rf ./KeyboardSwitch 2> /dev/null
