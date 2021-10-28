beddf8f5f2cc7312c76fcdff41f32e6d640f01c8
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-10-28 20:02:27 +0300

    Brush up the build scripts

--- Build-Portable.ps1
+++ Build-Portable.ps1
@@ -1,6 +1,6 @@
 if (Test-Path .\bin\KeyboardSwitch) 
 {
-    Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete()}
+    Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete() }
     Start-Sleep -Seconds 0.2
     Remove-Item -Path .\bin\KeyboardSwitch\* -Recurse -Force
 }
@@ -11,7 +11,7 @@ dotnet publish .\KeyboardSwitch --configuration Release --runtime win10-x64 --fr
 dotnet publish .\KeyboardSwitch.Settings --configuration Release --runtime win10-x64 --framework net5.0-windows `
 --self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:PublishTrimmed=true
 
-Remove-Item -Path .\bin\KeyboardSwitch\* -Include *.pdb, *.xml, *.dylib, *.so, *.so.*
+Remove-Item -Path .\bin\KeyboardSwitch\* -Include *.pdb, *.xml
 Remove-Item -Path .\bin\KeyboardSwitch\appsettings.macos.json
 Remove-Item -Path .\bin\KeyboardSwitch\appsettings.linux.json
 
@@ -22,7 +22,7 @@ if (Test-Path .\bin\KeyboardSwitch-Portable.zip)
 
 Compress-Archive -Path .\bin\KeyboardSwitch -DestinationPath .\bin\KeyboardSwitch-Portable.zip
 
-Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete()}
+Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete() }
 Start-Sleep -Seconds 0.2
 Remove-Item -Path .\bin\KeyboardSwitch\* -Recurse -Force
 Remove-Item -Path .\bin\KeyboardSwitch
