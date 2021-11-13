620167a7faf5b2c01b720588d1701857b2f27e4e
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2021-11-13 19:37:48 +0200

    Reenable trimming when publishing the app

--- KeyboardSwitch.Core/Util.cs
+++ KeyboardSwitch.Core/Util.cs
@@ -1,22 +1,20 @@
 namespace KeyboardSwitch.Core;
 
-using System.Runtime.InteropServices;
-
 public static class Util
 {
     public static T PlatformDependent<T>(Func<T> windows, Func<T> macos, Func<T> linux)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        if (OperatingSystem.IsWindows())
         {
             return windows();
         }
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        if (OperatingSystem.IsMacOS())
         {
             return macos();
         }
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        if (OperatingSystem.IsLinux())
         {
             return linux();
         }
