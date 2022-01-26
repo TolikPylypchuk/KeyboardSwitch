a71bd6ee1a9e66c409b8ae0d8bcc817196d3377d
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2022-01-26 23:34:13 +0200

    Add Arm64 to the script which builds the app's tar archive
     #69

--- build/tar/build-tar.sh
+++ build/tar/build-tar.sh
@@ -4,11 +4,41 @@ rm -rf ./bin/keyboard-switch 2> /dev/null
 mkdir -p ./bin
 cd ./bin
 
-dotnet publish ../KeyboardSwitch --configuration Release --runtime linux-x64 --framework net6.0 \
---self-contained true --output ./keyboard-switch --nologo -p:Platform=x64 -p:ContinuousIntegrationBuild=true
+while getopts p: opts; do
+   case ${opts} in
+      p) PLATFORM=${OPTARG};;
+   esac
+done
 
-dotnet publish ../KeyboardSwitch.Settings --configuration Release --runtime linux-x64 --framework net6.0 \
---self-contained true --output ./keyboard-switch --nologo -p:Platform=x64 -p:ContinuousIntegrationBuild=true
+if [ -z "$PLATFORM" ]
+then
+    PLATFORM="x64"
+fi
+
+PLATFORM=$(echo "$PLATFORM" | tr '[:upper:]' '[:lower:]')
+
+case $PLATFORM in
+    "arm64") RUNTIME="linux-arm64";;
+    *) RUNTIME="linux-x64";;
+esac
+
+case $PLATFORM in
+    "arm64") MSBUILD_PLATFORM="ARM64";;
+    *) MSBUILD_PLATFORM="x64";;
+esac
+
+case $PLATFORM in
+    "arm64") ARCH="arm64";;
+    *) ARCH="x64";;
+esac
+
+dotnet publish ../KeyboardSwitch --configuration Release --runtime "$RUNTIME" --framework net6.0 \
+--self-contained true --output ./keyboard-switch --nologo -p:Platform="$MSBUILD_PLATFORM" \
+-p:ContinuousIntegrationBuild=true
+
+dotnet publish ../KeyboardSwitch.Settings --configuration Release --runtime "$RUNTIME" --framework net6.0 \
+--self-contained true --output ./keyboard-switch --nologo -p:Platform="$MSBUILD_PLATFORM" \
+-p:ContinuousIntegrationBuild=true
 
 find ./keyboard-switch -name "*.pdb" -type f -delete
 find ./keyboard-switch -name "*.xml" -type f -delete
@@ -23,8 +53,8 @@ mv ./keyboard-switch/appsettings.linux.json ./keyboard-switch/appsettings.json
 cp ../build/tar/install.sh ./keyboard-switch
 cp ../build/tar/uninstall.sh ./keyboard-switch
 
-rm ./keyboard-switch-4.1-x64.tar.gz 2> /dev/null
+rm ./keyboard-switch-4.1-"$ARCH".tar.gz 2> /dev/null
 
-tar -czf ./keyboard-switch-4.1-x64.tar.gz ./keyboard-switch
+tar -czf ./keyboard-switch-4.1-"$ARCH".tar.gz ./keyboard-switch
 
 rm -rf ./keyboard-switch 2> /dev/null
