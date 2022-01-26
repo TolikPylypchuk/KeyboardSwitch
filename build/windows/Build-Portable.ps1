eb23acdb47fa55de175cedaf6ab7abbdd3b1806b
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2022-01-26 22:41:36 +0200

    Add Arm64 to the script which builds the app for Windows
    The build is experimental and not guaranteed to work since I don't have
    any devices with Arm64 CPUs.
    
    Unfortunately, .NET Framework 4.8 doesn't support Arm64, so the
    installer won't be available.

--- build/windows/Build-Portable.ps1
+++ build/windows/Build-Portable.ps1
@@ -1,15 +1,25 @@
+param([string]$Platform = "x64")
+
 if (Test-Path .\bin\KeyboardSwitch)
 {
-    Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete() }
+    Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | ForEach-Object { $_.Delete() }
     Start-Sleep -Seconds 0.2
     Remove-Item -Path .\bin\KeyboardSwitch\* -Recurse -Force
 }
 
-dotnet publish .\KeyboardSwitch --configuration Release --runtime win10-x64 --framework net6.0-windows `
---self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:ContinuousIntegrationBuild=true
+$Platform = $Platform.ToLower()
 
-dotnet publish .\KeyboardSwitch.Settings --configuration Release --runtime win10-x64 --framework net6.0-windows `
---self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=x64 -p:ContinuousIntegrationBuild=true
+$runtime = $Platform -eq "arm64" ? "win10-arm64" : "win10-x64"
+$msbuildPlatform = $Platform -eq "arm64" ? "ARM64" : "x64"
+$arch = $Platform -eq "arm64" ? "arm64" : "x64"
+
+dotnet publish .\KeyboardSwitch --configuration Release --runtime $runtime --framework net6.0-windows `
+--self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=$msbuildPlatform `
+-p:ContinuousIntegrationBuild=true
+
+dotnet publish .\KeyboardSwitch.Settings --configuration Release --runtime $runtime --framework net6.0-windows `
+--self-contained true --output .\bin\KeyboardSwitch --nologo -p:Platform=$msbuildPlatform `
+-p:ContinuousIntegrationBuild=true
 
 Remove-Item -Path .\bin\KeyboardSwitch\* -Include *.pdb, *.xml
 Remove-Item -Path .\bin\KeyboardSwitch\appsettings.macos.json
@@ -19,14 +29,14 @@ Remove-Item -Path .\bin\KeyboardSwitch\icon.png
 
 Rename-Item -Path .\bin\KeyboardSwitch\appsettings.windows.json -NewName appsettings.json
 
-if (Test-Path .\bin\KeyboardSwitch-4.1-x64-win.zip)
+if (Test-Path .\bin\KeyboardSwitch-4.1-$arch-win.zip)
 {
-    Remove-Item -Path .\bin\KeyboardSwitch-4.1-x64-win.zip
+    Remove-Item -Path .\bin\KeyboardSwitch-4.1-$arch-win.zip
 }
 
-Compress-Archive -Path .\bin\KeyboardSwitch -DestinationPath .\bin\KeyboardSwitch-4.1-x64-win.zip
+Compress-Archive -Path .\bin\KeyboardSwitch -DestinationPath .\bin\KeyboardSwitch-4.1-$arch-win.zip
 
-Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | foreach { $_.Delete() }
+Get-ChildItem -Path .\bin\KeyboardSwitch\* -File -Recurse | ForEach-Object { $_.Delete() }
 Start-Sleep -Seconds 0.2
 Remove-Item -Path .\bin\KeyboardSwitch\* -Recurse -Force
 Remove-Item -Path .\bin\KeyboardSwitch
