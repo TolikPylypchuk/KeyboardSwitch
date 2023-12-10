930d07b644432cbbfc4f7cce511a5db5de258006
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2023-12-10 18:20:01 +0200

    Setup NUKE
    GH-81

---
+++ build/KeyboardSwitch.Build/Build.cs
@@ -0,0 +1,24 @@
+public class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+}
