f915e02a77380dac91390826aa8aa4bfedd7a2b2
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-02-13 21:58:10 +0200

    Upgrade .NET

--- src/KeyboardSwitch.Core/Logging/SerilogLoggerFactory.cs
+++ src/KeyboardSwitch.Core/Logging/SerilogLoggerFactory.cs
@@ -14,19 +14,19 @@ public static class SerilogLoggerFactory
         configuration.GetRequiredSection("Logger").Bind(settings);
 
         return new LoggerConfiguration()
-                .MinimumLevel.Is(settings.MinimumLevel)
-                .WriteTo.Debug(outputTemplate: settings.OutputTemplate)
-                .WriteTo.Console(outputTemplate: settings.OutputTemplate)
-                .WriteTo.Logger(config => config
-                    .WriteTo.Async(writeTo => writeTo.File(
-                        Environment.ExpandEnvironmentVariables(settings.LogFilePath),
-                        outputTemplate: settings.OutputTemplate,
-                        fileSizeLimitBytes: settings.MaxFileSize,
-                        rollOnFileSizeLimit: true,
-                        retainedFileCountLimit: settings.MaxRetainedFiles,
-                        shared: true))
-                    /*.Filter.ByIncludingOnly(Matching.FromSource(nameof(KeyboardSwitch)))*/)
-                .Enrich.FromLogContext()
-                .CreateLogger();
+            .MinimumLevel.Is(settings.MinimumLevel)
+            .WriteTo.Debug(outputTemplate: settings.OutputTemplate)
+            .WriteTo.Console(outputTemplate: settings.OutputTemplate)
+            .WriteTo.Logger(config => config
+                .WriteTo.Async(writeTo => writeTo.File(
+                    Environment.ExpandEnvironmentVariables(settings.LogFilePath),
+                    outputTemplate: settings.OutputTemplate,
+                    fileSizeLimitBytes: settings.MaxFileSize,
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: settings.MaxRetainedFiles,
+                    shared: true))
+                .Filter.ByIncludingOnly(Matching.FromSource(nameof(KeyboardSwitch))))
+            .Enrich.FromLogContext()
+            .CreateLogger();
     }
 }
