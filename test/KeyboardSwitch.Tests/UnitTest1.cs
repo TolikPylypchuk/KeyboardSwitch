c099d92fdba2d8d255c3bcbc6b43f55ae0284930
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-02-25 13:11:55 +0200

    Add a project for tests
    GH-89

---
+++ test/KeyboardSwitch.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace KeyboardSwitch.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true); // Sanity check for the test infrastructure
+    }
+}
