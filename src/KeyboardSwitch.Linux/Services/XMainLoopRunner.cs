eaf7105d6d856d3a25eb19d5fdfeb3d0397f7942
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2024-05-11 18:16:38 +0300

    Refactor the main loop runner for X11

--- src/KeyboardSwitch.Linux/Services/XMainLoopRunner.cs
+++ src/KeyboardSwitch.Linux/Services/XMainLoopRunner.cs
@@ -1,73 +1,28 @@
 namespace KeyboardSwitch.Linux.Services;
 
-internal unsafe sealed class XMainLoopRunner : IMainLoopRunner
+internal unsafe sealed class XMainLoopRunner(X11Service x11, ILogger<XMainLoopRunner> logger) : IMainLoopRunner
 {
     private const int EPollTimeout = 1000;
 
-    private readonly X11Service x11;
-    private readonly ILogger<XMainLoopRunner> logger;
-
-    private readonly int epoll;
-    private readonly int sigread;
-    private readonly int sigwrite;
-
-    public XMainLoopRunner(X11Service x11, ILogger<XMainLoopRunner> logger)
-    {
-        this.x11 = x11;
-        this.logger = logger;
-
-        this.epoll = LibC.EPollCreate1(0);
-
-        if (this.epoll == -1)
-        {
-            throw new XException("epoll_create1 failed");
-        }
-
-        var fd = XLib.XConnectionNumber(x11.Display);
-        var ev = new EPollEvent()
-        {
-            Events = LibC.EPollIn,
-            Data = { U32 = (int)EventCode.X11 }
-        };
-
-        if (LibC.EPollCtl(this.epoll, LibC.EPollCtlAdd, fd, ref ev) == -1)
-        {
-            throw new XException("Unable to attach X11 connection handle to epoll");
-        }
-
-        var fds = stackalloc int[2];
-        LibC.Pipe2(fds, LibC.ONonBlock);
-        this.sigread = fds[0];
-        this.sigwrite = fds[1];
-
-        ev = new EPollEvent
-        {
-            Events = LibC.EPollIn,
-            Data = { U32 = (int)EventCode.Signal }
-        };
-
-        if (LibC.EPollCtl(this.epoll, LibC.EPollCtlAdd, this.sigread, ref ev) == -1)
-        {
-            throw new XException("Unable to attach signal pipe to epoll");
-        }
-    }
-
     public void RunMainLoop(CancellationToken token)
     {
-        this.logger.LogInformation("Running the main loop to listen for X11 events");
+        var (epoll, sigread) = this.InitializeEPoll();
+
+        logger.LogInformation("Running the main loop to listen for X11 events");
 
         while (!token.IsCancellationRequested)
         {
-            XLib.XFlush(this.x11.Display);
+            XLib.XFlush(x11.Display);
 
             EPollEvent ev;
 
-            if (XLib.XPending(this.x11.Display) == 0)
+            if (XLib.XPending(x11.Display) == 0)
             {
-                LibC.EPollWait(this.epoll, &ev, 1, EPollTimeout);
+                LibC.EPollWait(epoll, &ev, 1, EPollTimeout);
 
                 int buf = 0;
-                while (LibC.Read(this.sigread, &buf, 4) > 0)
+
+                while (LibC.Read(sigread, &buf, 4) > 0)
                 {
                     continue;
                 }
@@ -76,20 +31,63 @@ internal unsafe sealed class XMainLoopRunner : IMainLoopRunner
             if (!token.IsCancellationRequested)
             {
                 this.HandleX11Events(token);
+
             }
         }
     }
 
+    private (int EPoll, int SigRead) InitializeEPoll()
+    {
+        logger.LogInformation("Creating an epoll connection to listen for X11 events");
+
+        int epoll = LibC.EPollCreate1(0);
+
+        if (epoll == -1)
+        {
+            throw new XException("epoll_create1 failed");
+        }
+
+        var fd = XLib.XConnectionNumber(x11.Display);
+
+        var ev = new EPollEvent()
+        {
+            Events = LibC.EPollIn,
+            Data = { U32 = (int)EventCode.X11 }
+        };
+
+        if (LibC.EPollCtl(epoll, LibC.EPollCtlAdd, fd, ref ev) == -1)
+        {
+            throw new XException("Unable to attach X11 connection handle to epoll");
+        }
+
+        var fds = stackalloc int[2];
+        LibC.Pipe2(fds, LibC.ONonBlock);
+        int sigread = fds[0];
+
+        ev = new EPollEvent
+        {
+            Events = LibC.EPollIn,
+            Data = { U32 = (int)EventCode.Signal }
+        };
+
+        if (LibC.EPollCtl(epoll, LibC.EPollCtlAdd, sigread, ref ev) == -1)
+        {
+            throw new XException("Unable to attach signal pipe to epoll");
+        }
+
+        return (epoll, sigread);
+    }
+
     private unsafe void HandleX11Events(CancellationToken token)
     {
-        while (XLib.XPending(this.x11.Display) != 0)
+        while (XLib.XPending(x11.Display) != 0)
         {
             if (token.IsCancellationRequested)
             {
                 return;
             }
 
-            XLib.XNextEvent(this.x11.Display, out var xEvent);
+            XLib.XNextEvent(x11.Display, out var xEvent);
             if (XLib.XFilterEvent(ref xEvent, IntPtr.Zero))
             {
                 continue;
@@ -97,21 +95,21 @@ internal unsafe sealed class XMainLoopRunner : IMainLoopRunner
 
             if (xEvent.Type == XEventName.GenericEvent)
             {
-                XLib.XGetEventData(this.x11.Display, &xEvent.GenericEventCookie);
+                XLib.XGetEventData(x11.Display, &xEvent.GenericEventCookie);
             }
 
             try
             {
-                if (this.x11.TryGetEventHandler(xEvent.AnyEvent.Window, out var handler))
+                if (x11.TryGetEventHandler(xEvent.AnyEvent.Window, out var handler))
                 {
-                    this.logger.LogDebug("Handling an event from X11");
+                    logger.LogDebug("Handling an event from X11");
                     handler(ref xEvent);
                 }
             } finally
             {
                 if (xEvent.Type == XEventName.GenericEvent && xEvent.GenericEventCookie.Data != null)
                 {
-                    XLib.XFreeEventData(this.x11.Display, &xEvent.GenericEventCookie);
+                    XLib.XFreeEventData(x11.Display, &xEvent.GenericEventCookie);
                 }
             }
         }
