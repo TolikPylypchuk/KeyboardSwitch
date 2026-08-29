20461cafa708ab32a0f57138f0ffcdc9293fe0b4
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2026-08-29 19:27:00 +0300

    Use an event sequence template for event simulation

--- src/KeyboardSwitch.Core/Services/Simulation/SharpUserActivitySimulator.cs
+++ src/KeyboardSwitch.Core/Services/Simulation/SharpUserActivitySimulator.cs
@@ -7,33 +7,23 @@ public class SharpUserActivitySimulator(
 {
     private static readonly TimeSpan Delay = TimeSpan.FromMilliseconds(16);
 
+    private readonly IEventSimulationSequenceTemplate copyTemplate = eventSimulator.Sequence()
+        .AddKeyStroke(modifierKey, KeyCode.VcC)
+        .CreateTemplate();
+
+    private readonly IEventSimulationSequenceTemplate pasteTemplate = eventSimulator.Sequence()
+        .AddKeyStroke(modifierKey, KeyCode.VcV)
+        .CreateTemplate();
+
     public async Task SimulateCopy()
     {
-        eventSimulator.SimulateKeyPress(modifierKey);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyPress(KeyCode.VcC);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyRelease(KeyCode.VcC);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyRelease(modifierKey);
+        this.copyTemplate.Simulate();
         await scheduler.Sleep(Delay);
     }
 
     public async Task SimulatePaste()
     {
-        eventSimulator.SimulateKeyPress(modifierKey);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyPress(KeyCode.VcV);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyRelease(KeyCode.VcV);
-        await scheduler.Sleep(Delay);
-
-        eventSimulator.SimulateKeyRelease(modifierKey);
+        this.pasteTemplate.Simulate();
         await scheduler.Sleep(Delay);
     }
 }
